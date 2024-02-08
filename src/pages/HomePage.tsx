fcfce4eb27d08a35acb5019a4c4dd1a9d4215c3d
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-02-08 20:31:00 +0100

    testing h1 animation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
+import HeroHeader from '../components/HeroHeader'
 
 const HomePage = () => {
     return (
         <div className="hero min-h-screen bg-accent">
             <div className="hero-content text-center">
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold text-secondary">Hello there</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                <div className="max-w-lg">
+                    <HeroHeader />
+                    <button className="btn btn-primary mt-8">Get Started</button>
                     <button className="btn btn-secondary mx-4">Book</button>                   
                 </div>
             </div>
