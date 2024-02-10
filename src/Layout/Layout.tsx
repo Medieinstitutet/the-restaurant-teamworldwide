72459f9d85550107ed5b672e53032cb6b2d2f6e5
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-02-10 16:11:25 +0100

    strated with auth and protect admin route

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import { initializeApp } from "firebase/app"
+import { config } from '../config/config'
 
-
+//double check if this will only be called once, or if i need to move it somewhere else.
+initializeApp(config.firebaseConfig)
 
 const Layout = () => {
     return (
