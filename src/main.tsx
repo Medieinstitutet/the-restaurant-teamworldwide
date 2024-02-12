6c7556598f2548412eb9aff843c71dc17b40ec28
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-02-12 10:20:44 +0100

    Revert "Auth"

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './Router/Router.tsx'
-import { initializeApp } from "firebase/app";
-import { config } from './config/config.ts'
-
-
-
-initializeApp(config.firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
