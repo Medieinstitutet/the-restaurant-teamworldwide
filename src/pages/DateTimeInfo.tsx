cbfedfd6adf597fd5e8b295dfbf6bce8996e65d1
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-02-13 16:01:27 +0100

    started styling date-picker side

--- src/pages/DateTimeInfo.tsx
+++ src/pages/DateTimeInfo.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { Navigate, useNavigate } from 'react-router'
-import { Link } from 'react-router-dom'
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
+
 import dayjs, { Dayjs } from 'dayjs';
 import axios from 'axios';
-import { listClasses } from '@mui/material';
+import AddComponent from '../components/dateTimePicker';
 
 
 interface IRecievedBookings {
@@ -53,6 +51,7 @@ const DateTimeInfo = () => {
     }, [selectedDate])
 
     useEffect(() => {
+        if (!nineSelected) setTimeBooked("18:00")
         if (sixSelected) setTimeBooked("18:00")
         if (nineSelected) setTimeBooked("21:00")
     }, [sixSelected, nineSelected])
@@ -73,34 +72,48 @@ const DateTimeInfo = () => {
     return (
         <>
             <div className="w-100% bg-primary h-screen flex">
-                <div className='bg-black h-[100%] w-[30%] pt-32 text-neutral-50'>
+                <div className='bg-black h-[100%] w-[30%] pt-32 text-secondary'>
                     <h1 className='text-6xl text-center mt-12'>BLEU HORIZON</h1>
                     <h4 className='text-xl text-center'>GASTROPUB</h4>
-                    <div className='flex justify-center space-x-8 mt-20 pt-24'>
-                        <button disabled={fullyBooked18OnSelectedDate} onClick={() => handleBookSix()} className="btn btn-ghost text-xl bg-primary w-[40%]">{sixSelected ? "cancel six o clock" : "Six o clock"}</button>
-                        <button disabled={fullyBooked21OnSelectedDate} onClick={() => handleBookNine()} className="btn btn-ghost text-xl bg-primary w-[40%]">{nineSelected ? "cancel nine o clock" : "Nine o clock"}</button>
+                    <h4 className='text-4xl text-center mt-40'>Make your reservation today!</h4>
+                    <div className='flex justify-center space-x-8 mt-20 pt-24 text-white'>
+                        <button disabled={fullyBooked18OnSelectedDate} onClick={() => handleBookSix()} className="text-white btn text-l [w-40%] bg-primary">Six o clock</button>
+                        <button disabled={fullyBooked21OnSelectedDate} onClick={() => handleBookNine()} className="text-white btn text-l [w-40%] bg-primary">Nine o clock</button>
                     </div>
                 </div>
-                <div className=' bg-white pt-40 w-[70%]'>
-                    <DateCalendar className='text-primary text-8xl' value={selectedDate} onChange={(newValue) => setSelectedDate(newValue)} />
+                <div className=' bg-white pt-40 w-[70%] pl-20'>
 
+                    <AddComponent
+                        selectedDate={selectedDate}
+                        onChange={(newDate) => setSelectedDate(newDate)}
+                    />
+                    <div className='w-100'>
+                    <input type="range" min={0} max="100" value="25" className="range" step="25" />
+                    <div className="w-[50] flex justify-between text-xs px-2">
+                        <span>|</span>
+                        <span>|</span>
+                        <span>|</span>
+                        <span>|</span>
+                        <span>|</span>
+                    </div>
+                    </div>
+                    <div className='flex justify-center items-center space-x-4'>
+
+                        <h1 className='text-2xl text-center'>You're selecting: {selectedDataFormatted} at {timeBooked} </h1>
+                        <button className="text-white btn text-l [w-40%] bg-primary">Continue</button>
+                        <button className='btn self-center px-8 bg-primary hover:bg-neutral-50 text-neutral-50 hover:text-primary border-primary'>Send</button>
+                    </div>
                 </div>
+
             </div>
+
+
             <div>{fullyBooked18OnSelectedDate && "the restaurant is fully booked that day at 18"}</div>
             <div>{fullyBooked21OnSelectedDate && "the restaurant is fully booked that day at 21"}</div>
 
 
-            <div className='flex justify-center space-x-8'>
-                <button disabled={fullyBooked18OnSelectedDate} onClick={() => handleBookSix()} className="btn btn-ghost text-xl bg-slate-300">{sixSelected ? "cancel six o clock" : "choose six o clock"}</button>
-                <button disabled={fullyBooked21OnSelectedDate} onClick={() => handleBookNine()} className="btn btn-ghost text-xl bg-slate-300">{nineSelected ? "cancel nine o clock" : "choose nine o clock"}</button>
-            </div>
 
-            <h1>The current date reservation for API post is: {selectedDataFormatted}</h1>
-            <h1>The current time reservation for API post is: {timeBooked}</h1>
-            <form /* onSubmit={}  */ action="">
 
-                <Link to={"/contactinfo"}><button onClick={handleClick}>Click here to submit date time stuff</button></Link>
-            </form>
         </>
     )
 }
