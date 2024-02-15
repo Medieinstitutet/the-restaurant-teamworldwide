c72ec655f02746d4548baf09118a0c1c78075239
Jannie Hamberg <jg311209@live.com>
2024-02-15 23:29:27 +0100

    Fixed for xs screens

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -1,5 +1,5 @@
 export const MenuPage = () => {
-    return <div className="mt-16 px-10 py-20 sm:px-0 sm:py-10 bg-neutral-950">
+    return <div className="mt-16 px-10 py-20 sm:px-0 sm:py-10 2xs:px-0 2xs:py-0 bg-neutral-950">
         <img className="block mx-auto max-w-full" src="src\assets\White Brown Restaurant Menu.png" alt="" />
     </div>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '2xs': '375px',
+    },
+  },
   },
   plugins: [require("daisyui")],
   daisyui: {
