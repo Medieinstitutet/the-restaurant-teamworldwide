eb037fe76fd38ec7e18724e2a60cdd19309b46cb
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-02-16 13:10:48 +0100

    Added error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
 
 const ErrorPage = () => {
   return (
-    <div>Error</div>
+    <div>
+      <Navbar />
+        <div className="error min-h-screen mt-16 text-neutral-50 flex flex-col items-center pt-52 gap-10">
+          <h1>Page Not Found</h1>
+          <h3>Oops, it looks like this page does not exist.</h3>
+        </div>
+      <Footer />
+    </div>
   )
 }
 
