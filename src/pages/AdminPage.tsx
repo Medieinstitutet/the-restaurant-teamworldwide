0ea682d8aa7091feeee3c2af60465017ef0b9330
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-02-17 11:07:06 +0100

    working but bug which needs to be fixed

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -7,22 +7,26 @@ import {
   UserInputContext,
 } from "../contexts/userInputs";
 import { checkForAvailability } from "../helperfunctions/checkforavailbility";
+import { API_URL, EDIT_A_BOOKING, GET_ALL_BOOKINGS, RESTAURANT_ID } from "../constants/constants";
 
 const AdminPage = () => {
   const [bookings, setBookings] = useState<IReceivedBookings[]>([]);
   const [editedBooking, setEditedBooking] = useState<EditedBooking>(
-    new EditedBooking("", "65c6199912ebb6ed53265ac6", "", "", 1, "")
+    new EditedBooking("", RESTAURANT_ID, "", "", 1, "")
   );
   const [enableEdit, setEnableEdit] = useState(false);
-  const [newDate, setNewDate] = useState<Dayjs | null>(dayjs("2022-04-17"));
+  const [newDate, setNewDate] = useState<Dayjs>(dayjs("2022-04-17"));
   const [newDateFormatted, setnewDateFormatted] = useState("");
-  const [togglingDate, setTogglingDate] = useState(false);
+  const [fullyBookedAtSix, setFullyBookedAtSix] = useState(false)
+  const [fullyBookedAtNine, setFullyBookedAtNine] = useState(false)
+  const [enableDateChange, setEnableDateChange]= useState(false)
+
 
   useEffect(() => {
     const fetchAllBookings = async () => {
       try {
         const response = await axios.get<IReceivedBookings[]>(
-          "https://school-restaurant-api.azurewebsites.net/booking/restaurant/65c6199912ebb6ed53265ac6/"
+          ` ${API_URL}${GET_ALL_BOOKINGS}${RESTAURANT_ID}`
         );
         const allBookings = response.data;
         setBookings(allBookings);
@@ -34,10 +38,11 @@ const AdminPage = () => {
     fetchAllBookings();
   }, []);
 
+
   const deleteBooking = async (id: string) => {
     try {
       await axios.delete(
-        `https://school-restaurant-api.azurewebsites.net/booking/restaurant/${id}`
+        `https://school-restaurant-api.azurewebsites.net/booking/delete/${id}`
       );
       setBookings(bookings.filter((booking) => booking._id !== id));
     } catch (error) {
@@ -45,137 +50,107 @@ const AdminPage = () => {
     }
   };
 
-  const handleSelectChange = (
-    id: string,
-    customerId: string,
-    date: string,
-    time: string,
-    numberOfGuests: number,
-    e: ChangeEvent<HTMLSelectElement>
-  ) => {
-    if (e.target.name === "time") {
-      setEditedBooking(
-        new EditedBooking(
-          id,
-          "65c6199912ebb6ed53265ac6",
-          date,
-          e.target.value,
-          numberOfGuests,
-          customerId
-        )
-      );
+  useEffect(() => {
+    console.log(editedBooking)
 
-/*       checkForAvailability(
-        newDateFormatted
-      ); */
-/*       {
-        fullyBookedAtSix
-          ? alert(newDateFormatted + "18:00 fully booked")
-          : alert(newDateFormatted + "18:00 available");
-      }
-      {
-        fullyBookedAtNine
-          ? alert(newDateFormatted + "21:00 fully booked")
-          : alert(newDateFormatted + "21:00 available");
-      } */
-    } else if (e.target.name === "numberOfGuests") {
-      let numberOfGuests = +e.target.value;
-      setEditedBooking(
-        new EditedBooking(
-          id,
-          "65c6199912ebb6ed53265ac6",
-          date,
-          time,
-          numberOfGuests,
-          customerId
-        )
-      );
+    if(fullyBookedAtNine && editedBooking.time === "21:00") {
+      alert("We are fully booked at 21:00 this day, please choose another time or date")
+      return
+    }
+    if(fullyBookedAtSix && editedBooking.time === "18:00") {
+      alert("We are fully booked at 18:00 this day, please choose another time or date")
+      return
     }
 
-    setBookings(
-      bookings.map((booking) => {
-        if (booking._id === id) {
-          return { ...booking, [e.target.name]: e.target.value };
-        } else {
-          return booking;
-        }
-      })
-    );
-  };
+    const updateBooking = async () => {
+      try {
+        await axios.put<IReceivedBookings>(
+          `${API_URL}${EDIT_A_BOOKING}${editedBooking._id}`,
+          {
+            id: editedBooking._id,
+            restaurantId: RESTAURANT_ID,
+            date: editedBooking.date,
+            time: editedBooking.time,
+            numberOfGuests: editedBooking.numberOfGuests,
+            customerId: editedBooking.customerId,
+          }
+        )
+        console.log("update booking run");
+        const bookingUpdated = bookings.map((booking) => {
+        if(booking._id === editedBooking._id) {
+          return editedBooking
+        } return booking
+        })
+        setBookings(bookingUpdated)
+      } catch (error) {
+        error
+      }
+    }
+    updateBooking()
+  }, [editedBooking])
 
-  // useEffect(() => {
-  //   const newDateIsoString = newDate?.toISOString()
-  //   setnewDateFormatted(dayjs(newDateIsoString).format('YYYY-MM-DD'));
-  //   handleDateChange
-  // }, [newDate])
+
+  const handleDatechange = (newDate: Dayjs | null, _id: string) => {
+    if (newDate) {
+      const newDateToISO = newDate?.format('YYYY-MM-DD')
+      const bookingToEdit = bookings.find((booking) => booking._id === _id)
+      if (bookingToEdit)
+        setEditedBooking(new EditedBooking(bookingToEdit?._id, bookingToEdit?.restaurantId, newDateToISO, bookingToEdit?.time, bookingToEdit?.numberOfGuests, bookingToEdit?.customerId))
+    }
+  }
+
+  const handleTimechange = (_id: string, selectedValue: string) => {
+    console.log("selectedvalue" + selectedValue)
+    let newTime = selectedValue
+    const bookingToEdit = bookings.find((booking) => booking._id === _id)
+    if (bookingToEdit)
+      setEditedBooking(new EditedBooking(bookingToEdit?._id, bookingToEdit?.restaurantId, bookingToEdit.date, newTime, bookingToEdit?.numberOfGuests, bookingToEdit?.customerId))
+  }
+
+  const handleNumberOfPeopleChange = (_id: string, selectedValue: string) => {
+    let newNumberOfPeople = +selectedValue
+    const bookingToEdit = bookings.find((booking) => booking._id === _id)
+    if (bookingToEdit)
+      setEditedBooking(new EditedBooking(bookingToEdit?._id, bookingToEdit?.restaurantId, bookingToEdit.date, bookingToEdit.time, newNumberOfPeople, bookingToEdit?.customerId))
+  }
+
 
   useEffect(() => {
-    const newDateToISO = newDate?.toISOString();
-    setnewDateFormatted(dayjs(newDateToISO).format("YYYY-MM-DD"));
-    setTogglingDate(true);
-    if (newDateFormatted) {
-    }
-  }, [newDate]);
+    const updateCapacity = async (id: string) => {
 
-  const handleDateChange = (
-    date: string,
-    id: string,
-    customerId: string,
-    time: string,
-    numberOfGuests: number
-  ) => {
-    console.log(date);
-    console.log(newDateFormatted);
-    setEditedBooking(
-      new EditedBooking(
-        id,
-        "65c6199912ebb6ed53265ac6",
-        newDateFormatted,
-        time,
-        numberOfGuests,
-        customerId
+      const bookingsAt18and21 = checkForAvailability(
+        editedBooking.date, bookings
       )
-    );
-    setBookings(
-      bookings.map((booking) => {
-        if (booking._id === id) {
-          return { ...booking, date: newDateFormatted };
-        } else {
-          return booking;
-        }
-      })
-    );
-    setEnableEdit(!enableEdit);
-  };
+      const bookingsAt18 = bookingsAt18and21[0]
+      const bookingsAt21 = bookingsAt18and21[1]
+      console.log("bookings at 18 on current date = " + bookingsAt18.length)
+      console.log("bookings at 21 on current date = " + bookingsAt21.length)
 
-  const updateBooking = async (id: string) => {
-    if (editedBooking !== undefined) {
-      await axios.put<IReceivedBookings>(
-        `https://school-restaurant-api.azurewebsites.net/booking/update/${id}`,
-        {
-          id: id,
-          restaurantId: "65c6199912ebb6ed53265ac6",
-          date: editedBooking.date,
-          time: editedBooking.time,
-          numberOfGuests: editedBooking.numberOfGuests,
-          customerId: editedBooking.customerId,
-        }
-      );
+      if (bookingsAt18.length >= 16) {
+        setFullyBookedAtSix(true)
+      } else {
+        (bookingsAt18.length <= 16)
+        setFullyBookedAtSix(false)
+      }
+      if (bookingsAt21.length >= 16) {
+        setFullyBookedAtNine(true)
+      } else {
+        (bookingsAt21.length <= 16)
+        setFullyBookedAtNine(false)
+      }
     }
-    console.log("update booking has been run");
-  };
+    updateCapacity(editedBooking._id)
+  }), [editedBooking.date]
+
+
+
+
+
+
 
   const toggleEnableEdit = (id: string) => {
     setEnableEdit(!enableEdit);
-    if (enableEdit) {
-      updateBooking(id);
-      console.log(`booking with ${id} has been updated`);
-    }
   };
-
-  useEffect(() => {
-    console.log(bookings);
-  }, [enableEdit]);
 
   return (
     <div className="mt-16 min-h-screen bg-white">
@@ -209,29 +184,12 @@ const AdminPage = () => {
               <td className='px-5 py-2 text-center'>{booking.numberOfGuests}</td>
               <td className='px-5 py-2'>{booking.customerId}</td> */}
               <td className="px-5 py-2">
-                {togglingDate ? (
-                  <button
-                    id="edit-button"
-                    onClick={() =>
-                      handleDateChange(
-                        newDateFormatted,
-                        booking._id,
-                        booking.customerId,
-                        booking.time,
-                        booking.numberOfGuests
-                      )
-                    }
-                  >
-                    {enableEdit ? "Save" : "Edit"}
-                  </button>
-                ) : (
-                  <button
-                    id="edit-button"
-                    onClick={() => toggleEnableEdit(booking._id)}
-                  >
-                    {enableEdit ? "Save" : "Edit"}
-                  </button>
-                )}
+                <button
+                  id="edit-button"
+                  onClick={() => toggleEnableEdit(booking._id)}
+                >
+                  {enableEdit ? "Save" : "Edit"}
+                </button>
               </td>
               <td className="px-5 py-2">{booking._id}</td>
               <td className="px-5 py-2">
@@ -240,28 +198,21 @@ const AdminPage = () => {
                   disabled={!enableEdit}
                   name="date"
                   value={dayjs(booking.date)}
-                  onChange={(newDate) => setNewDate(newDate)}
+                  onChange={(newDate) => handleDatechange(newDate, booking._id)}
                 />
               </td>
               <td className="px-5 py-2 flex justify-center gap-1">
                 <select
-                  disabled={!enableEdit}
+                  disabled={enableDateChange}
                   className="select select-bordered w-max max-w-xs font"
                   name="time"
                   value={booking.time}
                   onChange={(e) =>
-                    handleSelectChange(
-                      booking._id,
-                      booking.customerId,
-                      booking.date,
-                      booking.time,
-                      booking.numberOfGuests,
-                      e
-                    )
+                    handleTimechange(booking._id, e.target.value)
                   }
                 >
-                  <option>18:00</option>
-                  <option>21:00</option>
+                  <option value={"18:00"}>18:00</option>
+                  <option value={"21:00"}>21:00</option>
                 </select>
               </td>
               <td className="px-5 py-2 text-center">
@@ -271,14 +222,7 @@ const AdminPage = () => {
                   name="numberOfGuests"
                   value={booking.numberOfGuests}
                   onChange={(e) =>
-                    handleSelectChange(
-                      booking._id,
-                      booking.customerId,
-                      booking.date,
-                      booking.time,
-                      booking.numberOfGuests,
-                      e
-                    )
+                    handleNumberOfPeopleChange(booking._id, e.target.value)
                   }
                 >
                   <option>1</option>
