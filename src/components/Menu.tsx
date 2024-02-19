1bfa18fa3c2ea1c157e39c35d0a3211647341195
Jannie Hamberg <jg311209@live.com>
2024-02-19 08:57:48 +0100

    Added tween transition on menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,44 @@
 import { Link } from "react-router-dom"
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+import {useEffect } from 'react'
+
+const variants = {
+    hidden: {opacity: 0, y: 60 },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type:'tween',
+            ease: 'easeInOut',
+            duration: 3.8,
+        }
+    },
+};
+    
 
 export const Menu = () => {
+    const controls = useAnimation();
+    const [ref, inView] = useInView({
+        threshold: 0.1,
+        triggerOnce: true,
+    });
+
+
+useEffect(() => {
+    if (inView) {
+        controls.start('visible');
+    }
+}, [controls, inView]);
+
     return (
+    <motion.div
+        ref={ref}
+        initial='hidden'
+        animate={controls}
+        variants={variants}
+      /*   transition={{ duration: 0.9 }} */
+        >
         <div className="menu min-h-full bg-neutral-950">
             <h1 className="text-5xl text-neutral-50 text-center p-14">ON THE MENU</h1>
             <div className="menu-content">
@@ -31,5 +68,6 @@ export const Menu = () => {
                 </figure>
             </div>
         </div>
+    </motion.div>
     )
 }
