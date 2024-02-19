c720c1ded3d3f54a297bb94f958f549973af7498
Jannie Hamberg <jg311209@live.com>
2024-02-19 09:12:17 +0100

    switch back to initial value on opacity

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,7 +4,7 @@ import { useInView } from 'react-intersection-observer'
 import {useEffect } from 'react'
 
 const variants = {
-    hidden: {opacity: 0.1, y: 60 },
+    hidden: {opacity: 0, y: 60 },
     visible: {
         opacity: 1,
         y: 0,
