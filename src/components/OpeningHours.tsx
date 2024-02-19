41ba0d4d2867428273d17061d86c9cc144311da3
Jannie Hamberg <jg311209@live.com>
2024-02-19 09:24:15 +0100

    Fixed identical animation on openinghrs component

--- src/components/OpeningHours.tsx
+++ src/components/OpeningHours.tsx
@@ -1,7 +1,48 @@
 import { Button } from "./Button"
+import { Link } from "react-router-dom"
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+import {useEffect } from 'react'
+
+
+const variants = {
+    hidden: {opacity: 0, y: 60 },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type:'tween',
+            ease: 'easeInOut',
+            duration: 3.8,
+        }
+    },
+};
+    
 
 export const OpeningHours = () => {
+    const controls = useAnimation();
+    const [ref, inView] = useInView({
+        threshold: 0.1,
+        triggerOnce: true,
+    });
+
+
+useEffect(() => {
+    if (inView) {
+        controls.start('visible');
+    }
+}, [controls, inView]);
+
+
+
     return (
+        <motion.div
+        ref={ref}
+        initial='hidden'
+        animate={controls}
+        variants={variants}
+      /*   transition={{ duration: 0.9 }} */
+        >
         <div className="opening-hours-cont relative">
             <Button linkTo={"/booking"} children={"Book now"} className={"absolute right-10 top-10"} size={"lg"} color={"light"}/>
             <img className="opening-hours pb-10" src="src\assets\openinghrs.png" alt="opening hours" />
@@ -18,5 +59,6 @@ export const OpeningHours = () => {
             </div>
             <img className="logo-2 pb-10 pr-10 self-end" src="src\assets\opening_hours-text.svg" alt="Where great food and great drinks meet" /> */}
         </div>
+        </motion.div>
     )
 }
